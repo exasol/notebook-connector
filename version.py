8462dc7d277693e4ae710ffa445ea8d7e12662a1
ckunki <christoph.kuhnke@exasol.com>
2023-10-13 11:58:22 +0200

    Added file version.py and fixed reference

---
+++ version.py
@@ -0,0 +1,7 @@
+# ATTENTION:
+# This file is generated, do not edit it manually!
+# If you need to change the version, do so in the project.toml, e.g. by using `poetry version X.Y.Z`.
+MAJOR = 0
+MINOR = 0
+PATCH = 1
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
