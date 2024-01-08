4eecb7f3193f3bbba2b502b441c53e7d630bbf91
Maxim Lapan <maxim.lapan@exasol.com>
2024-01-08 12:30:33 +0100

    BFS bucket mocking

--- test/unit/test_bfs_utils.py
+++ test/unit/test_bfs_utils.py
@@ -1,11 +1,47 @@
 import pytest
 from unittest import mock
+from typing import Generator
 import pathlib
 import exasol.bucketfs as bfs
 from exasol import bfs_utils
 
 
+MOCKED_BUCKET = "bucket"
+MOCKED_FILE_NAME = "bfs.file"
+
+
 @mock.patch("exasol.bucketfs.Bucket")
 def test_put_file_basic(bfs_bucket: mock.MagicMock):
     with pytest.raises(ValueError, match="Local file doesn't exist"):
-        bfs_utils.put_file(bfs_bucket, pathlib.Path("non/existent.file"))
+        bfs_utils.put_file(bfs_bucket, pathlib.Path("non/existent/local.file"))
+
+
+@pytest.fixture
+@mock.patch("exasol.bucketfs.Bucket")
+def bucket_with_file(bfs_bucket: mock.MagicMock):
+    bfs_bucket.name = MOCKED_BUCKET
+    bfs_bucket.__iter__.return_value = iter([MOCKED_FILE_NAME])
+    bfs_bucket.upload.return_value = None
+    return bfs_bucket
+
+
+@pytest.fixture
+def temp_file(tmp_path) -> Generator[pathlib.Path, None, None]:
+    p = pathlib.Path(tmp_path) / MOCKED_FILE_NAME
+    p.write_text("data")
+    yield p
+    p.unlink()
+
+
+def test_put_file_exists(caplog, bucket_with_file, temp_file):
+    caplog.set_level("INFO")
+    p = bfs_utils.put_file(bucket_with_file, temp_file)
+    assert str(p) == f"/buckets/bfsdefault/{MOCKED_BUCKET}/{MOCKED_FILE_NAME}"
+    assert "already present in the bucketfs" in caplog.text
+    assert not bucket_with_file.upload.called
+
+    caplog.clear()
+    p = bfs_utils.put_file(bucket_with_file, temp_file, skip_if_exists=False)
+    assert str(p) == f"/buckets/bfsdefault/{MOCKED_BUCKET}/{MOCKED_FILE_NAME}"
+    assert bucket_with_file.upload.called
+    assert "Uploading file" in caplog.text
