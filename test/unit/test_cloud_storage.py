f6f66525d299e4c769f10015fc3a91b311577e84
Maxim Lapan <maxim.lapan@exasol.com>
2024-01-24 14:25:51 +0100

    Add checks for classes from jar

--- test/unit/test_cloud_storage.py
+++ test/unit/test_cloud_storage.py
@@ -9,5 +9,8 @@ def test_setup_scripts(secrets):
     assert mock_db_conn.execute.call_count == 4
     assert "OPEN SCHEMA" in mock_db_conn.execute.mock_calls[0].args[0]
     assert "CREATE OR REPLACE JAVA SET SCRIPT IMPORT_PATH" in mock_db_conn.execute.mock_calls[1].args[0]
+    assert "com.exasol.cloudetl.scriptclasses.FilesImportQueryGenerator" in mock_db_conn.execute.mock_calls[1].args[0]
     assert "CREATE OR REPLACE JAVA SCALAR SCRIPT IMPORT_METADATA" in mock_db_conn.execute.mock_calls[2].args[0]
+    assert "com.exasol.cloudetl.scriptclasses.FilesMetadataReader" in mock_db_conn.execute.mock_calls[2].args[0]
     assert "CREATE OR REPLACE JAVA SET SCRIPT IMPORT_FILES" in mock_db_conn.execute.mock_calls[3].args[0]
+    assert "com.exasol.cloudetl.scriptclasses.FilesDataImporter" in mock_db_conn.execute.mock_calls[3].args[0]
