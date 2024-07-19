2be5218475c0f76848a9efc0805c759ecdada214
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2024-07-19 07:21:34 -0300

    Renamed default alias

--- exasol/nb_connector/slct_manager.py
+++ exasol/nb_connector/slct_manager.py
@@ -14,7 +14,7 @@ from exasol.nb_connector.ai_lab_config import AILabConfig as CKey, AILabConfig
 from exasol.nb_connector.language_container_activation import ACTIVATION_KEY_PREFIX
 from exasol.nb_connector.secret_store import Secrets
 
-DEFAULT_ALIAS = "ai-lab-default"
+DEFAULT_ALIAS = "ai_lab_default"
 PATH_IN_BUCKET = "container"
 
 # Activation SQL for the Custom SLC will be saved in the secret
