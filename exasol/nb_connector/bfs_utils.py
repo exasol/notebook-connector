5241057f77237c3fe5ce971d79236bad65b4a2de
Maxim Lapan <maxim.lapan@exasol.com>
2024-12-16 12:07:09 +0100

    Handle the SaasBucket specifics

--- exasol/nb_connector/bfs_utils.py
+++ exasol/nb_connector/bfs_utils.py
@@ -9,6 +9,22 @@ import exasol.bucketfs as bfs  # type: ignore
 _logger = logging.getLogger(__name__)
 
 
+def _file_in_bucket(file_name: str, bucket: bfs.Bucket) -> bool:
+    """
+    Checks that given file name is present in bucket.
+    :param file_name: name to check
+    :param bucket: bucket object
+    :return: True if name is present, else False
+    """
+    try:
+        return file_name in list(bucket)
+    except TypeError as e:
+        # SaaSBucket is not iterable, need to handle it here
+        if e.args[0].endswith("not iterable"):
+            return False
+        raise e
+
+
 def put_file(bucket: bfs.Bucket, file_path: pathlib.Path,
              skip_if_exists: bool = True) -> pathlib.Path:
     """
@@ -21,7 +37,7 @@ def put_file(bucket: bfs.Bucket, file_path: pathlib.Path,
     if not file_path.exists():
         raise ValueError(f"Local file doesn't exist: {file_path}")
     local_name = file_path.name
-    if skip_if_exists and local_name in list(bucket):
+    if skip_if_exists and _file_in_bucket(local_name, bucket):
         _logger.info("File %s is already present in the bucketfs", local_name)
     else:
         _logger.info("Uploading file %s to bucketfs", local_name)
