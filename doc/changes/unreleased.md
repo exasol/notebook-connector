abd2f371a17c65879bd2fb354b31ed3b1d12b48a
Ariel Schulz <ariel.schulz@exasol.com>
2025-05-08 11:16:15 +0200

    Update to exasol-toolbox 1.1.0

--- doc/changes/unreleased.md
+++ doc/changes/unreleased.md
@@ -3,3 +3,7 @@
 ## Security Issues
 
 * #183: Updated h11 to 0.16.0, updated transformers to 4.50.0
+
+## Refactoring
+
+* #183: Updated to exasol-toolbox 1.1.0
--- noxconfig.py
+++ noxconfig.py
@@ -11,6 +11,7 @@ class Config:
     doc: Path = ROOT_DIR / "doc"
     version_file: Path = ROOT_DIR / "version.py"
     path_filters: Iterable[str] = ("dist", ".eggs", "venv")
+    python_versions = ["3.10", "3.11", "3.12", "3.13"]
 
 
 PROJECT_CONFIG = Config()
