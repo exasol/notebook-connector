8a7118296169f88173e592dbf36a5931be21d94f
ckunki <christoph.kuhnke@exasol.com>
2025-07-16 16:53:09 +0200

    Removed experimental test implementations

--- test/integration/test_text_ai_extension_wrapper.py
+++ test/integration/test_text_ai_extension_wrapper.py
@@ -23,8 +23,6 @@ from exasol.ai.text.impl.utils.sql_utils import (
     varchar_column_spec,
 )
 from exasol.analytics.schema import (
-    Column,
-    ColumnName,
     SchemaName,
     Table,
     TableName,
@@ -42,20 +40,6 @@ from exasol.nb_connector.text_ai_extension_wrapper import (
 )
 
 
-def test_text_ai_extension_1(secrets: Secrets, setup_itde):
-    conf = secrets
-    # this test is very slow and downloads and unpacks a 6 GB file, so we only
-    # execute it in the manually triggered slow tests
-    language_alias = f"PYTHON3_TXAIE_TEST"
-    initialize_text_ai_extension(conf, language_alias=language_alias)
-    with open_pyexasol_connection(conf) as pyexasol_connection:
-        activate_languages(pyexasol_connection, conf)
-        assert_run_empty_udf(language_alias, pyexasol_connection, conf)
-        assert_connection_exists(
-            conf.get(CKey.txaie_bfs_connection), pyexasol_connection
-        )
-
-
 class TxaiTest:
     def __init__(self, schema_name: str):
         self.schema = SchemaName(schema_name)
@@ -68,32 +52,6 @@ class TxaiTest:
     def input_table_name(self, id: int = 0) -> TableName:
         suffix = f"_{id}" if id else ""
         return TableNameImpl(table_name=f"INPUT_TABLE{suffix}", schema=self.schema)
-
-
-# import pytest
-# import pyexasol
-# @pytest.fixture(scope="session")
-# def pyexasol_connection():
-#     c = pyexasol.connect(dsn="192.168.124.221:8563", user="sys", password="exasol")
-#     return c
-
-
-def x1_test_x1(pyexasol_connection):
-    topics = {"urgent", "not urgent"}
-    scenario = TxaiTest("INPUT_SCHEMA")
-    schema = scenario.schema
-    table = scenario.table
-    pyexasol_connection.execute(
-        f"DROP SCHEMA IF EXISTS {schema.fully_qualified} CASCADE"
-    )
-    pyexasol_connection.execute(f"CREATE SCHEMA {schema.fully_qualified}")
-    pyexasol_connection.execute(table.create_statement)
-    pyexasol_connection.execute(
-        f"INSERT INTO {table.name.fully_qualified} VALUES"
-        " (1, 'This is a test.'),"
-        " (2, 'Exasol is awesome.')"
-    )
-    print(f"{table.name.name} {scenario.text_column.name.name} {schema.name}")
 
 
 def test_text_ai_extension_2(secrets: Secrets, setup_itde):
