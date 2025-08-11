e2d27b3e6a778767b8b5578079dc9acd04af6ffc
ckunki <christoph.kuhnke@exasol.com>
2025-08-11 16:13:46 +0200

    Update SLCR release

--- exasol/nb_connector/slc/constants.py
+++ exasol/nb_connector/slc/constants.py
@@ -18,7 +18,7 @@ FLAVORS_PATH_IN_SLC_REPO = Path("flavors")
 
 SLC_RELEASE_TAG = "9.7.0"
 """
-Using the SLC_RELEASE 9.6.0 because we are limited to slc-tool 3.*. (see pyproject.toml)
+Using the SLC_RELEASE 9.7.0 because we are limited to slc-tool 3.*. (see pyproject.toml)
 Check the developer guide (./doc/developer-guide.md) for more information.
 """
 
