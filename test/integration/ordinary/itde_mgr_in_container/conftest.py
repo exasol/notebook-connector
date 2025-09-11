a4481665ae32b07773b6244e01e4ee613e531ba9
ckunki <christoph.kuhnke@exasol.com>
2025-09-11 12:01:55 +0200

    Added optional dependency for itests with external ITDE (3)
    Added optional dependency ibis

--- test/integration/ordinary/itde_mgr_in_container/conftest.py
+++ test/integration/ordinary/itde_mgr_in_container/conftest.py
@@ -343,7 +343,7 @@ def docker_container(
             copy.add_string_to_file("itde_stop_and_restart.py", itde_stop_and_restart)
             copy.add_string_to_file("itde_external_test.py", itde_external_test)
             copy.copy("/tmp")
-            extras = "docker-db,bucketfs"
+            extras = "docker-db,bucketfs,ibis"
             exit_code, output = container.exec_run(
                 f'python3 -m pip install "/tmp/{wheel_path.name}[{extras}]"'
                 f" --extra-index-url https://download.pytorch.org/whl/cpu"
