21b3676e5ab84be5b1abd4a8b22aaa8fefa82f35
ckunki <christoph.kuhnke@exasol.com>
2025-10-06 09:05:44 +0200

    Added first try for CLI integration tests

---
+++ test/integration/ordinary/itest_cli.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+from click.testing import CliRunner
+from exasol.pytest_backend.itde import (
+    OnpremBfsConfig,
+    OnpremDBConfig,
+)
+
+from exasol.nb_connector.cli import commands
+
+
+def test_roundtrip_onprem(
+    use_onprem: bool,
+    sample_db_file: Path,
+    exasol_config: OnpremDBConfig,
+    bucketfs_config: OnpremBfsConfig,
+    monkeypatch: MonkeyPatch,
+) -> None:
+    if not use_onprem:
+        pytest.skip("This test requires an on-premise database")
+
+    secrets = {
+        "SCS_MASTER_PASSWORD": "abc",
+        "SCS_EXASOL_DB_PASSWORD": exasol_config.password,
+        "SCS_BUCKETFS_PASSWORD": bucketfs_config.password,
+    }
+    for env_var, value in secrets.items():
+        monkeypatch.setitem(os.environ, env_var, value)
+
+    scs_file = str(sample_db_file)
+    bfs_url = bucketfs_config.url.split(":")
+    result = CliRunner().invoke(commands.configure, [
+        "onprem"
+        "--db-host-name", exasol_config.host,
+        "--db-port", str(exasol_config.port),
+        "--db-username", exasol_config.username,
+        "--db-password", # from env
+        "--db-use-encryption",  # TODO: verify!
+        "--bucketfs-host", bfs_url[0],
+        "--bucketfs-port", bfs_url[1],
+        "--bucketfs-user", bucketfs_config.username,
+        "--bucketfs-password", # from env
+        "--bucketfs-name", "bfsdefault",
+        "--bucket", "default",
+        "--no-bucketfs-use-encryption",
+        "--no-ssl-use-cert-validation",
+        "--db-schema", "SSS",
+        scs_file,
+    ])
+    assert result.exit_code == 0
+    result = CliRunner().invoke(commands.check, [scs_file])
+    assert result.exit_code == 0
