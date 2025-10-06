2ed1977a068143f6fea97a44de894b1ba36d10ee
ckunki <christoph.kuhnke@exasol.com>
2025-10-06 14:10:33 +0200

    Added iTest for SaaS

--- test/integration/ordinary/itest_cli.py
+++ test/integration/ordinary/itest_cli.py
@@ -2,6 +2,7 @@ import os
 from collections.abc import Iterator
 from pathlib import Path
 from test.utils.integration_test_utils import sample_db_file
+from typing import Any
 from urllib.parse import urlparse
 
 import pytest
@@ -21,13 +22,47 @@ def scs_file_path() -> Iterator[Path]:
         yield file
 
 
+def test_roundtrip_saas(
+    use_saas: bool,
+    scs_file_path: Path,
+    saas_host: str,
+    saas_pat: str,
+    saas_account_id: str,
+    backend_aware_saas_database_id: str,
+    monkeypatch: MonkeyPatch,
+) -> None:
+    if not use_saas:
+        pytest.skip("This test requires an Exasol SaaS instance")
+
+    secrets = {
+        "SCS_FILE": str(scs_file_path),
+        "SCS_MASTER_PASSWORD": "abc",
+        "SCS_EXASOL_SAAS_TOKEN": saas_pat,
+    }
+    for env_var, value in secrets.items():
+        monkeypatch.setitem(os.environ, env_var, value)
+
+    scs_file = str(scs_file_path)
+    result = CliRunner().invoke(commands.configure, [
+        "saas",
+         "--saas-url", saas_host,
+         "--saas-account-id", saas_account_id,
+         "--saas-database-id", backend_aware_saas_database_id,
+         "--saas-token", # from env
+         "--db-schema", "SSS",
+    ])
+    assert result.exit_code == 0
+    result = CliRunner().invoke(commands.check, ["--connect"])
+    assert result.exit_code == 0
+
+
 def test_roundtrip_onprem(
     use_onprem: bool,
     scs_file_path: Path,
     exasol_config: OnpremDBConfig,
     bucketfs_config: OnpremBfsConfig,
     monkeypatch: MonkeyPatch,
-    backend_aware_database_params,
+    backend_aware_database_params: dict[str, Any],
 ) -> None:
     if not use_onprem:
         pytest.skip("This test requires an on-premise database")
@@ -35,10 +70,10 @@ def test_roundtrip_onprem(
     print(f"DB Config: {exasol_config}")
     print(f"BFS Config: {bucketfs_config}")
     secrets = {
+        "SCS_FILE": str(scs_file_path),
         "SCS_MASTER_PASSWORD": "abc",
         "SCS_EXASOL_DB_PASSWORD": exasol_config.password,
         "SCS_BUCKETFS_PASSWORD": bucketfs_config.password,
-        "SCS_FILE": str(scs_file_path),
     }
     for env_var, value in secrets.items():
         monkeypatch.setitem(os.environ, env_var, value)
