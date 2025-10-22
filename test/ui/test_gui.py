cae4335b0b8c0663fef5625582c23fd0ef70449a
Janaarthanan Selvarajan <janaarthanan.selvarajan@exasol.com>
2025-10-22 14:07:25 +0000

    removed unwanted comments

--- test/ui/test_gui.py
+++ test/ui/test_gui.py
@@ -2,13 +2,13 @@ import ipywidgets as widgets
 import playwright.sync_api
 import solara
 from IPython.display import display
+import ipyvuetify
+
 
 
 def test_widget_button_solara(
     solara_test, page_session: playwright.sync_api.Page, assert_solara_snapshot
 ):
-    # this all runs in process, which only works with solara
-    # also, this test is only with pure ipywidgets
     button = widgets.Button(description="Click Me!")
 
     def change_description(obj):
@@ -20,8 +20,6 @@ def test_widget_button_solara(
     button_sel.wait_for()
     button_sel.click()
     page_session.locator("text=Tested event").wait_for()
-    # with ipyvuetify 2 we modified the button font for ipywidgets, that does not happen anymore with ipyvuetify 3
-    # therefore we have a different screenshot for ipyvuetify 2 and 3
     assert_solara_snapshot(
         page_session.locator("text=Tested event").screenshot(),
         postfix="-ipyvuetify3" if solara.util.ipyvuetify_major_version == 3 else "",
