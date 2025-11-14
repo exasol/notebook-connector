0808bdb675c5cbfaeb10ef18d221c785204e8ebe
MarleneKress79789 <marlene.kress@gmx.de>
2025-11-14 14:08:54 +0100

    ignore type, fix pyproject.toml

--- test/integration/saas_instance/test_cloud_storage.py
+++ test/integration/saas_instance/test_cloud_storage.py
@@ -21,7 +21,7 @@ from exasol.nb_connector.secret_store import Secrets
 def test_cloud_storage_setup_scripts(secrets: Secrets, setup_itde):
     local_jar_path = retrieve_jar(Project.CLOUD_STORAGE_EXTENSION)
     bucket = open_bucketfs_bucket(secrets)
-    bfs_jar_path = put_file(bucket, local_jar_path)
+    bfs_jar_path = put_file(bucket, local_jar_path)  # type: ignore
     udf_jar_path = bfs_jar_path.as_udf_path()
     assert udf_jar_path.startswith("/buckets/")
     with open_pyexasol_connection(secrets) as db_conn:
