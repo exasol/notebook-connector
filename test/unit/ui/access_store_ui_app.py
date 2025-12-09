53b7bca204db7da5446166eca24821e29432e07c
jana-selva <janaarthanan.selvarajan@exasol.com>
2025-12-09 18:49:10 +0530

    read and write for store files using magic store command

---
+++ test/unit/ui/access_store_ui_app.py
@@ -0,0 +1,86 @@
+"""
+UI app for the Access Store
+"""
+
+from pathlib import Path
+
+import ipywidgets as widgets
+from IPython import get_ipython
+
+from exasol.nb_connector.secret_store import (
+    InvalidPassword,
+    Secrets,
+)
+from exasol.nb_connector.ui.popup_message_ui import popup_message
+from exasol.nb_connector.ui.ui_styles import get_config_styles
+
+# pylint-disable and type-ignore are added in this file because this code
+# is intended to be run in jupyter notebook
+
+# pylint: disable=global-variable-undefined
+
+DEFAULT_FILE_NAME = "ai_lab_secure_configuration_storage.sqlite"
+
+ui_look = get_config_styles()
+
+# File path input
+file_txt = widgets.Text(
+    value=DEFAULT_FILE_NAME, style=ui_look.input_style, layout=ui_look.input_layout
+)
+
+# Password input
+password_txt = widgets.Password(style=ui_look.input_style, layout=ui_look.input_layout)
+
+# UI Elements
+header_lbl = widgets.Label(
+    value="Configuration Store",
+    style=ui_look.header_style,
+    layout=ui_look.header_layout,
+)
+file_lbl = widgets.Label(
+    value="File Path", style=ui_look.label_style, layout=ui_look.label_layout
+)
+password_lbl = widgets.Label(
+    value="Password", style=ui_look.label_style, layout=ui_look.label_layout
+)
+open_btn = widgets.Button(
+    description="Open", style=ui_look.button_style, layout=ui_look.button_layout
+)
+
+
+def open_or_create_config_store(btn):
+    global ai_lab_config
+    sb_store_file = file_txt.value
+    ipython = get_ipython()
+    try:
+        ai_lab_config = Secrets(Path(sb_store_file), password_txt.value)
+        ai_lab_config.connection()
+    except InvalidPassword:
+        popup_message(
+            "Failed to open the store. Please check that the password is correct"
+        )
+    else:
+        open_btn.icon = "check"
+    finally:
+        # Save the file path variable to the shared store (IPython only)
+        if ipython and hasattr(ipython, "run_line_magic"):
+            ipython.run_line_magic("store", "sb_store_file")
+        del sb_store_file
+
+
+def on_value_change(change):
+    open_btn.icon = "pen"
+
+
+open_btn.on_click(open_or_create_config_store)
+file_txt.observe(on_value_change, names=["value"])
+password_txt.observe(on_value_change, names=["value"])
+
+
+group_items = [
+    header_lbl,
+    widgets.Box([file_lbl, file_txt], layout=ui_look.row_layout),
+    widgets.Box([password_lbl, password_txt], layout=ui_look.row_layout),
+]
+items = [widgets.Box(group_items, layout=ui_look.group_layout), open_btn]
+app = widgets.Box(items, layout=ui_look.outer_layout)
---
+++ test/unit/ui/test_access_store_ui_app.py
@@ -0,0 +1,65 @@
+import importlib.resources
+import logging
+from test.unit.ui.access_store_ui_app import DEFAULT_FILE_NAME
+from test.unit.ui.util import solara_app_utils as solara
+from unittest.mock import (
+    MagicMock,
+)
+
+import ipywidgets
+import pytest
+from solara.server import reload
+
+logger = logging.getLogger("solara.server.access_store_app_test")
+
+ACCESS_STORE_APP_SRC = (
+    importlib.resources.files("test.unit.ui") / "access_store_ui_app.py"
+)
+reload.reloader.start()
+
+NEW_FILE_NAME = "new_file.sqlite"
+TEST_PASSWORD = "password"
+
+
+@pytest.fixture
+def store_with_file(monkeypatch, tmp_path):
+    monkeypatch.chdir(tmp_path)
+
+    def _store_with_file(filename):
+        mocked_ipython = MagicMock()
+        mocked_ipython.run_line_magic.side_effect = lambda magic, key: (
+            filename if magic == "store" and key == "sb_store_file" else None
+        )
+        monkeypatch.setattr("IPython.get_ipython", lambda: mocked_ipython)
+        return mocked_ipython
+
+    return _store_with_file
+
+
+def test_store_read(kernel_context, no_kernel_context, store_with_file):
+    """Test for the store magic read"""
+    store = store_with_file(NEW_FILE_NAME)
+    with solara.app_box_and_rc(ACCESS_STORE_APP_SRC, kernel_context) as (box, rc):
+        text = rc.find(ipywidgets.Text)[0].widget
+        text.value = store.run_line_magic("store", "sb_store_file")
+        assert text.value == NEW_FILE_NAME
+
+
+def test_store_write(kernel_context, no_kernel_context, store_with_file):
+    """Test for the store magic write"""
+    store = store_with_file(DEFAULT_FILE_NAME)
+    with solara.app_box_and_rc(ACCESS_STORE_APP_SRC, kernel_context) as (box, rc):
+        text = rc.find(ipywidgets.Text)[0].widget
+        password = rc.find(ipywidgets.Password).widget
+        button = rc.find(ipywidgets.Button).widget
+
+        assert isinstance(text, ipywidgets.Text)
+        assert text.value == DEFAULT_FILE_NAME
+
+        text.value = NEW_FILE_NAME
+        password.value = TEST_PASSWORD
+        assert button.icon == "pen"
+
+        button.click()
+        store.run_line_magic.assert_called_with("store", "sb_store_file")
+        assert button.icon == "check"
