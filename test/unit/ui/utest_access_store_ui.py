0a073f5c570e09f9ce0311f1042e79816c329e9e
jana-selva <janaarthanan.selvarajan@exasol.com>
2025-12-22 11:11:26 +0530

    305 - unit test modified to support normal file instead of store file

--- test/unit/ui/utest_access_store_ui.py
+++ test/unit/ui/utest_access_store_ui.py
@@ -2,36 +2,37 @@
 Unit tests for access store read and write functions.
 """
 
-from unittest.mock import (
-    MagicMock,
-    patch,
-)
-
-import ipywidgets
-
-from exasol.nb_connector.ui.access_store_ui import get_access_store_ui
+import exasol.nb_connector.ui.access_store_ui as access_ui
 
 
-def test_access_store_ui_store_read_and_write(tmp_path):
+def test_access_store_ui_store_read_and_write_2(tmp_path, monkeypatch):
     """
-    Test for access store read and write functionality.
+    Test access store UI reading and writing SCS file path via widgets and helper functions.
     """
-    mock_ipython = MagicMock()
-    with patch(
-        "exasol.nb_connector.ui.access_store_ui.get_ipython", return_value=mock_ipython
-    ):
-        # call UI
-        ui = get_access_store_ui(str(tmp_path))
-        # assert "store -r" is called
-        mock_ipython.run_line_magic.assert_any_call("store", "-r")
-        # find open button
-        open_btn = next(
-            child
-            for child in ui.children
-            if isinstance(child, ipywidgets.Button) and child.description == "Open"
-        )
-        # click open button
-        mock_ipython.run_line_magic.reset_mock()
-        open_btn.click()
-        # assert "store" is called
-        mock_ipython.run_line_magic.assert_any_call("store", "sb_store_file")
+    monkeypatch.chdir(tmp_path)
+    monkeypatch.setattr(
+        access_ui,
+        "get_scs_location_file_path",
+        lambda: test_scs_file
+    )
+
+    test_scs_file = tmp_path / "scs_file"
+
+    if test_scs_file.exists():
+        test_scs_file.unlink()
+
+    assert access_ui.get_sb_store_file() == access_ui.DEFAULT_FILE_NAME
+    # open UI
+    ui = access_ui.get_access_store_ui(str(tmp_path))
+    # select the textfield and enter file name
+    file_name = ui.children[0].children[1].children[1]
+    file_name.value = "test_config.sqlite"
+    # select the password field and enter password
+    password = ui.children[0].children[2].children[1]
+    password.value = "password"
+    # click the open button
+    open_button = ui.children[1]
+    open_button.click()
+    # assert the file name
+    assert test_scs_file.read_text().strip() == "test_config.sqlite"
+    assert access_ui.get_sb_store_file() == "test_config.sqlite"
