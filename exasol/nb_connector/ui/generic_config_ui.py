b2ff13164de8dd2dffe711aa353fd2c1d07faeaf
jana-selva <janaarthanan.selvarajan@exasol.com>
2026-02-03 15:27:28 +0530

    added tests for value change and save

---
+++ exasol/nb_connector/ui/generic_config_ui.py
@@ -0,0 +1,89 @@
+from itertools import chain
+
+import ipywidgets as widgets
+
+from exasol.nb_connector.ai_lab_config import AILabConfig as CKey
+from exasol.nb_connector.secret_store import Secrets
+from exasol.nb_connector.ui.ui_styles import get_config_styles
+
+
+def get_generic_config_ui(
+    secrets: Secrets,
+    inputs: list[list[tuple[str, widgets.Widget, CKey]]],
+    group_names: list[str],
+) -> widgets.Widget:
+    """
+    The function creates a generic configuration editor UI.
+    The UI consists of one or more blocks of configuration data aligned vertically.
+    Each block has a header and one or more rows with input fields. Each row has a
+    label on the left and one input widget on the right. The form ends with a "Save"
+    button, clicking on which results in saving the data in the configuration store.
+
+    Parameters
+        conf:        Configuration store
+        inputs:      List of the input blocks. Each block is a list of input rows.
+                     Each row consists of the label text, input widget, and the key
+                     of the corresponding configuration element in the store.
+        group_names: Header names for the blocks. The length of this list should
+                     match the length of the inputs.
+    """
+
+    ui_look = get_config_styles()
+    save_btn = widgets.Button(
+        description="Save", style=ui_look.button_style, layout=ui_look.button_layout
+    )
+
+    def save_configuration(btn):
+        for row in chain(*inputs):
+            _, widget, key = row
+            secrets.save(key, str(widget.value))
+        btn.icon = "check"
+
+    def on_value_change(change):
+        save_btn.icon = "pen"
+
+    save_btn.on_click(save_configuration)
+
+    # Apply the styles and layouts to the input fields
+    for row in chain(*inputs):
+        widget = row[1]
+        widget.style = ui_look.input_style
+        widget.layout = ui_look.input_layout
+        widget.observe(on_value_change, names=["value"])
+    # Create a list of lists with input rows
+    item_groups = [
+        [
+            widgets.Box(
+                [
+                    widgets.Label(
+                        value=input_title,
+                        style=ui_look.label_style,
+                        layout=ui_look.label_layout,
+                    ),
+                    input_widget,
+                ],
+                layout=ui_look.row_layout,
+            )
+            for input_title, input_widget, _ in input_group
+        ]
+        for input_group in inputs
+    ]
+    # Create a list of blocks
+    items = [
+        widgets.Box(
+            [
+                widgets.Label(
+                    value=group_name,
+                    style=ui_look.header_style,
+                    layout=ui_look.header_layout,
+                )
+            ]
+            + item_group,
+            layout=ui_look.group_layout,
+        )
+        for item_group, group_name in zip(item_groups, group_names)
+    ]
+    # Add the save button and put everything in an outer Box.
+    items.append(save_btn)
+    ui = widgets.Box(items, layout=ui_look.outer_layout)
+    return ui
--- test/integration/ui/test_main_config_ui.py
+++ test/integration/ui/test_main_config_ui.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from typing import Optional
 
 from IPython.display import display
+from playwright.sync_api import expect
 
 from exasol.nb_connector.secret_store import Secrets
 from exasol.nb_connector.ui.main_config_ui import get_db_config_ui, get_db_selection_ui
@@ -26,6 +28,7 @@ def render_main_config_ui(page_session, conf):
     page_session.wait_for_timeout(1000)
     return ui
 
+
 def render_db_selection_ui(page_session, conf):
     ui = get_db_selection_ui(conf=conf)
     display(ui)
@@ -40,6 +43,8 @@ def assert_config_ui_screenshot(assert_solara_snapshot, page_session):
         anchor_selector="button:text('Save')",
         parent_levels=1,
     )
+
+
 def assert_selection_ui_screenshot(assert_solara_snapshot, page_session):
     assert_ui_screenshot(
         assert_solara_snapshot,
@@ -49,12 +54,116 @@ def assert_selection_ui_screenshot(assert_solara_snapshot, page_session):
     )
 
 
+def set_text_input(
+        row,
+        *,
+        value: Optional[str] = None,
+        clear: bool = False,
+        text_to_type: Optional[str] = None,
+):
+    """
+    Update a text input located inside a given row.
+    """
+    inp = row.locator("input")
+    if clear:
+        inp.clear()
+    if value is not None:
+        inp.fill(value)
+    if text_to_type is not None:
+        inp.type(text_to_type)
+
+
+def row_by_label(page_session, label: str):
+    """
+    Locate the row/container element corresponding to a labeled field.
+
+    """
+    return page_session.locator(f"text={label}").locator("..")
+
+
+def save_button(page_session):
+    """
+    Locate the 'Save' button.
+    """
+    return page_session.locator("button:text('Save')")
+
+
+def click_save(page_session):
+    """
+    Click the 'Save' button in the UI.
+    """
+    page_session.locator("button:text('Save')").click()
+    print("clicked Save button")
+    page_session.wait_for_timeout(2000)
+
+
+def expect_pen_icon(page_session, count: int = 1):
+    """
+    Assert the presence/absence count of the 'pen' icon inside the Save button.
+    """
+    expect(save_button(page_session).locator("i.fa-pen")).to_have_count(count)
+
+
+def expect_check_icon(page_session, count: int = 1):
+    """
+    Assert the presence/absence count of the 'pen' icon inside the Save button.
+    """
+    expect(save_button(page_session).locator("i.fa-check")).to_have_count(count)
+
+
 def test_default_main_config_ui_load(
         solara_test, page_session, assert_solara_snapshot, tmp_path
 ):
     conf = create_conf(tmp_path)
     render_main_config_ui(page_session, conf)
     assert_config_ui_screenshot(assert_solara_snapshot, page_session)
+
+
+def test_default_main_config_load_default_on_clear(
+        solara_test, page_session, assert_solara_snapshot, tmp_path
+):
+    conf = create_conf(tmp_path)
+    render_main_config_ui(page_session, conf)
+    set_text_input(row_by_label(page_session, "Memory Size (GiB)"), clear=True)
+    set_text_input(row_by_label(page_session, "Disk Size (GiB)"), clear=True)
+    assert_config_ui_screenshot(assert_solara_snapshot, page_session)
+
+
+def test_default_main_config_pen_icon_on_change(
+        solara_test, page_session, assert_solara_snapshot, tmp_path
+):
+    conf = create_conf(tmp_path)
+    render_main_config_ui(page_session, conf)
+    set_text_input(row_by_label(page_session, "Memory Size (GiB)"), text_to_type="4")
+    page_session.wait_for_timeout(1000)
+    page_session.keyboard.press("Tab")
+    expect_pen_icon(page_session, 1)
+    # TODO: cannot see pen icon in the screenshot, need to find a solution for that
+    page_session.wait_for_timeout(3000)
+    assert_config_ui_screenshot(assert_solara_snapshot, page_session)
+
+
+def test_default_main_config_check_icon_on_save(
+        solara_test, page_session, assert_solara_snapshot, tmp_path
+):
+    conf = create_conf(tmp_path)
+    render_main_config_ui(page_session, conf)
+    set_text_input(row_by_label(page_session, "Memory Size (GiB)"), value="3")
+    click_save(page_session)
+    page_session.wait_for_timeout(5000)
+    expect_check_icon(page_session, 1)
+    assert_config_ui_screenshot(assert_solara_snapshot, page_session)
+
+
+def test_default_main_config_check_icon_on_save_direct(
+        solara_test, page_session, assert_solara_snapshot, tmp_path
+):
+    conf = Secrets(db_file=Path(tmp_path / "sample.sqlite"), master_password="password")
+    render_main_config_ui(page_session, conf)
+    set_text_input(row_by_label(page_session, "Memory Size (GiB)"), value="3")
+    page_session.locator("button:text('Save')").click()
+    expect_check_icon(page_session, 1)
+
 
 def test_onprem_main_config_ui_load(
         solara_test, page_session, assert_solara_snapshot, tmp_path
@@ -64,6 +173,7 @@ def test_onprem_main_config_ui_load(
     render_main_config_ui(page_session, conf)
     assert_config_ui_screenshot(assert_solara_snapshot, page_session)
 
+
 def test_saas_main_config_ui_load(
         solara_test, page_session, assert_solara_snapshot, tmp_path
 ):
@@ -72,6 +182,45 @@ def test_saas_main_config_ui_load(
     render_main_config_ui(page_session, conf)
     assert_config_ui_screenshot(assert_solara_snapshot, page_session)
 
+
+def test_saas_ui_pen_on_change(
+        solara_test, page_session, assert_solara_snapshot, tmp_path
+):
+    conf = create_conf(tmp_path)
+    conf.save(CKey.storage_backend, StorageBackend.saas.name)
+    render_main_config_ui(page_session, conf)
+    set_text_input(row_by_label(page_session, "Account ID"), value="account_id")
+    set_text_input(row_by_label(page_session, "Database ID"), value="database_id")
+    expect_pen_icon(page_session, 1)
+    """
+    TODO: cannot see pen icon in the screenshot, 
+    but playwright is able detect the icon in the above line of code 
+    `expect_pen_icon(page_session, 1)`
+    need to find a solution for that and enable screenshot asserion
+    """
+    # assert_config_ui_screenshot(assert_solara_snapshot, page_session)
+
+
+def test_saas_ui_check_on_save(
+        solara_test, page_session, assert_solara_snapshot, tmp_path
+):
+    conf = create_conf(tmp_path)
+    conf.save(CKey.storage_backend, StorageBackend.saas.name)
+    render_main_config_ui(page_session, conf)
+    set_text_input(row_by_label(page_session, "Account ID"), value="account_id")
+    set_text_input(row_by_label(page_session, "Database ID"), value="database_id")
+    expect_pen_icon(page_session, 1)
+    """
+    TODO: cannot see pen icon in the screenshot, 
+    but playwright is able detect the icon in the above line of code 
+    `expect_pen_icon(page_session, 1)`
+    need to find a solution for that and enable screenshot asserion
+    """
+    click_save(page_session)
+    # expect_check_icon(page_session, 1)
+    assert_config_ui_screenshot(assert_solara_snapshot, page_session)
+
+
 def test_itde_main_config_ui_load(
         solara_test, page_session, assert_solara_snapshot, tmp_path
 ):
@@ -79,6 +228,7 @@ def test_itde_main_config_ui_load(
     conf.save(CKey.use_itde, "True")
     render_main_config_ui(page_session, conf)
     assert_config_ui_screenshot(assert_solara_snapshot, page_session)
+
 
 def test_default_db_selection_ui_load(
         solara_test, page_session, assert_solara_snapshot, tmp_path
