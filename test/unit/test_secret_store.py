cf8df48f35f715adbb430993f9406d5f810dba0d
ckunki <christoph.kuhnke@exasol.com>
2026-02-11 17:18:07 +0100

    Added load test for multiple threads

--- test/unit/test_secret_store.py
+++ test/unit/test_secret_store.py
@@ -1,4 +1,9 @@
+import logging
+import random
 import sqlite3
+import threading
+import time
+from dataclasses import dataclass
 from pathlib import Path
 
 import pytest
@@ -8,6 +13,9 @@ from exasol.nb_connector.secret_store import (
     InvalidPassword,
     Secrets,
 )
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 
 
 @pytest.fixture
@@ -136,12 +144,77 @@ def test_remove_db_schema(secrets):
 
 def test_performance(secrets):
     """
-    range(0, 20):
-    old implementation: 0.24 seconds
-    new implementation 7.9 seconds (3% speed)
+    duration of range(0, 20) in seconds:
+    0.24 old implementation
+    7.90 separate connection (32 times slower)
+    0.14 connection pool (runtime 58 %)
     """
     for i in range(0, 20):
         key = f"key-{i}"
         value = f"value {i}"
         secrets.save(key, value)
         assert value == secrets.get(key)
+
+
+def access_scs(secrets: Secrets):
+    for i in range(0, 5):
+        print(f'{i+1} {secrets}')
+        time.sleep(0.1)
+
+@dataclass
+class Result:
+    iteration: int
+    key: str
+    written: str
+    read: str
+
+    def __str__(self) -> str:
+        if self.written == self.read:
+            return (
+                f'Iteration {self.iteration} successfully used key {self.key}'
+                f' to write and read value {self.written}.'
+            )
+        return (
+            f'Iteration {self.iteration} used key {self.key},'
+            f' wrote {self.written}, but read {self.read}.'
+        )
+
+
+class AccessThread(threading.Thread):
+    def __init__(self, id: str, secrets: Secrets, sleep: int = 0, iterations: int = 0):
+        super().__init__(target=self.access_scs)
+        self.id = id
+        self._secrets = secrets
+        self._sleep = sleep or random.randint(1, 10) / 100
+        self._iterations = iterations or random.randint(4, 8)
+        self.results: list[Result] = []
+
+    @property
+    def random_key(self) -> str:
+        i = random.randint(0, 2) + 1
+        return f'K{i}'
+
+    def access_scs(self):
+        for i in range(0, self._iterations):
+            time.sleep(self._sleep)
+            key = self.random_key
+            written = self.id
+            self._secrets.save(key, written)
+            time.sleep(self._sleep)
+            read = self._secrets.get(key)
+            result = Result(i, key, written, read)
+            self.results.append(result)
+            LOG.info(f'thread {self.id}: {result}')
+
+def test_multithreads(secrets):
+    """
+    Verify multiple threads concurrently accessing the same secret store.
+    """
+    threads = [AccessThread(f"T{i}", secrets) for i in range(0,20)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    for t in threads:
+        for r in t.results:
+            assert r.written == r.read, f'Thread {t.id}: {r}'
