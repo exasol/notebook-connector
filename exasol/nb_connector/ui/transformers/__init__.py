cd70c7268cf4869c6f13409206ed0c1eb0ee8ab1
Janaarthanan Selvarajan <janaarthanan.selvarajan@exasol.com>
2026-04-09 15:56:29 +0530

    #379: Converted `ai-lab/te_init_ui.ipynb` to `py` and added unit and integration tests (#380)
    * #379: Converted `ai-lab/te_init_ui.ipynb` to `py` and added unit and integration tests

---
+++ exasol/nb_connector/ui/transformers/__init__.py
@@ -0,0 +1,8 @@
+"""Transformers-related UI components.
+
+This package exposes public helpers for configuring transformer-related UIs.
+"""
+
+from . import util
+
+__all__ = ["util"]
---
+++ exasol/nb_connector/ui/transformers/util/te_init.py
@@ -0,0 +1,24 @@
+"""
+Hugging Face credentials UI
+This is not supposed to be used on its own.
+"""
+
+import ipywidgets as widgets
+
+from exasol.nb_connector.ai_lab_config import AILabConfig as CKey
+from exasol.nb_connector.secret_store import Secrets
+from exasol.nb_connector.ui.config import generic
+
+
+def get_hf_config(ai_lab_config: Secrets) -> widgets.Widget:
+    input_rows = [
+        (
+            "Access token",
+            widgets.Password(value=ai_lab_config.get(CKey.huggingface_token)),
+            CKey.huggingface_token,
+        )
+    ]
+
+    return generic.get_config(
+        ai_lab_config, [input_rows], ["Hugging Face Access Parameters"]
+    )
