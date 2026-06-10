77ea236c0e2eff80c0937129e2762b994cd7f189
Torsten Kilias <tkilias@users.noreply.github.com>
2026-06-10 17:43:42 +0200

    #421: Enable SaaS SLC deploy (#422)

--- test/notebooks/test_script_languages_container.py
+++ test/notebooks/test_script_languages_container.py
@@ -1,25 +1,23 @@
 import os
 import shutil
+import time
 from pathlib import Path
 from test.integration.ui.common.utils.notebook_test_utils import run_notebook
 
 import pytest
-from exasol.pytest_backend import BACKEND_ONPREM
 
 
 @pytest.fixture()
 def cleanup_slc_repo_dir(backend, notebooks_root):
     yield
-    if backend == BACKEND_ONPREM:
-        p = notebooks_root / "script_languages_container" / "slc_workspace"
+    p = notebooks_root / "script_languages_container" / "slc_workspace"
+    if p.exists():
         shutil.rmtree(p)
 
 
 def test_script_languages_container(
     backend, backend_setup, cleanup_slc_repo_dir, notebooks_root
 ) -> None:
-    if backend != BACKEND_ONPREM:
-        pytest.skip()
     current_dir = Path.cwd()
     store_path, store_password = backend_setup
     store_file = str(store_path)
@@ -30,6 +28,10 @@ def test_script_languages_container(
         run_notebook("configure_slc_repository.ipynb", store_file, store_password)
         run_notebook("export_as_is.ipynb", store_file, store_password)
         run_notebook("customize.ipynb", store_file, store_password)
+        # The sleep is needed because SaaS takes long for the synchronization of the SLC
+        # and at the moment we don't have a better way to wait for it.
+        if backend == "saas":
+            time.sleep(10 * 60)
         run_notebook("test_slc.ipynb", store_file, store_password)
         run_notebook("advanced.ipynb", store_file, store_password)
         run_notebook(
